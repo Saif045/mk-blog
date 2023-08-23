423c66baf6116dc15af579a7c0841a1b166c12b9
Saif045 <saif.x45@gmail.com>
2023-08-23 08:06:20 +0300

    .

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -12,7 +12,12 @@ export default async function og() {
   return new ImageResponse(
     (
       <div tw="relative w-full h-full flex items-center justify-center">
-        <Image src={"/hero.webp"} alt="Hero-img" fill />
+        <img
+          width={900}
+          height={450}
+          src="https://deepbluembedded.com/wp-content/uploads/2021/04/DeepBlue-Website-Hero-Background-e1618552512933.jpg"
+          alt="Hero Image"
+        />
         <div tw="absolute flex bg-black opacity-50 inset-0 " />
         <p tw="text-white text-4xl flex text-center font-bold m-5">Home</p>
       </div>
