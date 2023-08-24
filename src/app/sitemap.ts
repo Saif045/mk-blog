72c57be43188f179e11e8e04bf7fea24555d94c0
Saif045 <saif.x45@gmail.com>
2023-08-24 07:07:13 +0300

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,41 @@
+import { getPostSlugs } from "@/lib/posts";
+import { getSeo } from "@/lib/seo";
+
+export default async function sitemap() {
+  const baseUrl = "https://mk-blog-45.vercel.app";
+
+  ///
+  const posts = await getPostSlugs("posts");
+  let postModifiedTime: string;
+  for (const post of posts) {
+    const seo = await getSeo(post.slug);
+    postModifiedTime = seo.opengraphModifiedTime;
+  }
+  const postUrls = posts.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: postModifiedTime,
+  }));
+
+  ///
+  const projects = await getPostSlugs("projects");
+  let projectModifiedTime: string;
+  for (const project of projects) {
+    const seo = await getSeo(project.slug);
+    projectModifiedTime = seo.opengraphModifiedTime;
+  }
+  const projectUrls = projects.map((project) => ({
+    url: `${baseUrl}/projects/${project.slug}`,
+    lastModified: projectModifiedTime,
+  }));
+
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/blog`, lastModified: new Date() },
+    { url: `${baseUrl}/contact`, lastModified: new Date() },
+    { url: `${baseUrl}/projects`, lastModified: new Date() },
+
+    ...postUrls,
+    ...projectUrls,
+  ];
+}
