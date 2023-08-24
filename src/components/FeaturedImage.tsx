6a7ee89510b0a9b3050c242c30dfb57ee0dcfc62
Saif045 <saif.x45@gmail.com>
2023-08-24 09:34:17 +0300

    .

--- src/components/FeaturedImage.tsx
+++ src/components/FeaturedImage.tsx
@@ -34,7 +34,7 @@ export default function FeaturedImage({
         width={img.width}
         height={img.height}
         alt={post.title}
-        className="h-[200px] w-[300px]  object-cover rounded-xl"
+        className="h-[200px] w-[300px]  object-cover rounded-xl border border-black dark:border-white"
       />
     </Link>
   );
